394f1473cf6c4a84b32df85ac15d655fa6ca7f5e
kademola <kademola517@gmail.com>
2024-04-02 19:15:04 +0100

    This is my initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+    // Author: Kayode Ademola Festus
+	// Author URI: linkedin.com/kaydeewrld
+	// Support: King-Manuel
+	// Version: 1.1
+    // Programming Language Used: JAVASCRIPT, CSS3, HTML 5, BOOTSTRAP 5.2 
+    // DATE: 30/03/2024 - 01/04/2024
