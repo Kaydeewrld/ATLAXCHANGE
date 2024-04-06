7c7aeee07c12ec8bf7eed88cb4960927d4adcd76
kademola <kademola517@gmail.com>
2024-04-06 10:58:44 +0100

    this is my second commmit

--- script.js
+++ script.js
@@ -4,3 +4,14 @@
 	// Version: 1.1
     // Programming Language Used: JAVASCRIPT, CSS3, HTML 5, BOOTSTRAP 5.2 
     // DATE: 30/03/2024 - 01/04/2024
+    var swiper = new Swiper(".swiper", {
+        effect: "cards",
+        grabCursor: true,
+        initialSlide: 2,
+        speed: 500,
+        loop: true,
+        rotate: true,
+        mousewheel: {
+        invert: false,
+      },
+    });
